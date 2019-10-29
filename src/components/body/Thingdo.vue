f5f5d1122d892bc0d7d5886403ce4a3284c81a85
Mr.Giraffe <gleemansc@163.com>
2019-10-29 10:04:48 +0800

    安装测试mock
    没怎么用过mock模拟数据，简单试下，在Thingdo.vue文件下create生命周期里

--- src/components/body/Thingdo.vue
+++ src/components/body/Thingdo.vue
@@ -26,6 +26,7 @@
 </template>
 
 <script>
+import Mock from 'mockjs'
 export default {
   name: "",
   components: {},
@@ -55,7 +56,15 @@ export default {
       }
     }
   },
-  created () { },
+  created () {
+    console.log(Mock.mock({
+      // 属性 list 的值是一个数组，其中含有 1 到 10 个元素
+      'list|1-10': [{
+        // 属性 id 是一个自增数，起始值为 1，每次增 1
+        'id|+1': 1
+      }]
+    }))
+  },
   mounted () { }
 };
 </script>
